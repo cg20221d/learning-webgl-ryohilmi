b2a22b6479226e4adf3765188a51ef3ab5bb0ace
Ryo Hilmi <ryodm.74@gmail.com>
2022-09-14 15:23:56 +0700

    add gl assembly

--- main.js
+++ main.js
@@ -2,11 +2,24 @@ function main() {
   let canvas = document.getElementById("kanvas");
   let gl = canvas.getContext("webgl");
 
+  let vertices = [
+    0.5, 0.5,
+    0.0, 0.0,
+    -0.5, 0.5,
+    0.0, 1.0
+  ];
+
+  let buffer = gl.createBuffer();
+  gl.bindBuffer(gl.ARRAY_BUFFER, buffer);
+  gl.bufferData(gl.ARRAY_BUFFER, new Float32Array(vertices), gl.STATIC_DRAW);
+
   // Vertex shader
   const vertexShaderCode = `
+    attribute vec2 aPosition;
+
     void main() {
-      float x = 0.0;
-      float y = 0.0;
+      float x = aPosition.x;
+      float y = aPosition.y;
       gl_PointSize = 10.0;
       gl_Position = vec4(x, y, 0.0, 1.0);
     }
@@ -37,8 +50,15 @@ function main() {
   gl.linkProgram(shaderProgram);
   gl.useProgram(shaderProgram);
 
+  // Mengajari GPU bagaimana caranya mengoleksi
+  // nilai posisi dari ARRAY_BUFFER
+  // untuk setiap verteks yang sedang diproses
+  let aPosition = gl.getAttribLocation(shaderProgram, "aPosition");
+  gl.vertexAttribPointer(aPosition, 2, gl.FLOAT, false, 0, 0);
+  gl.enableVertexAttribArray(aPosition);
+
   gl.clearColor(1.0, 0.65, 0.0, 1.0);
   gl.clear(gl.COLOR_BUFFER_BIT);
 
-  gl.drawArrays(gl.POINTS, 0, 1);
+  gl.drawArrays(gl.LINES, 0, 4);
 }
